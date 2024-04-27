b1eca6f4bc2c565fe04be0ed7f6c3c4e54d80abe
AryanMishra2k <167567736+AryanMishra2k@users.noreply.github.com>
2024-04-27 16:07:47 +0000

    updated code

--- grmodel.py
+++ grmodel.py
@@ -1,19 +1,29 @@
-from gramformer import Gramformer
-import torch
+from transformers import T5ForConditionalGeneration, T5Tokenizer
 
-gf = Gramformer(models = 1, use_gpu=False)
-
+model = T5ForConditionalGeneration.from_pretrained("Unbabel/gec-t5_small")
+tokenizer = T5Tokenizer.from_pretrained('t5-small')
 
 def correct(sentence):
-    res = gf.correct(sentence) # Gramformer correct
-    return res[0] # Return first value in res array
+    tokenized_sentence = tokenizer('gec: ' + sentence, max_length=128, truncation=True, padding='max_length', return_tensors='pt')
+    corrected_sentence = tokenizer.decode(
+        model.generate(
+            input_ids = tokenized_sentence.input_ids,
+            attention_mask = tokenized_sentence.attention_mask,
+            max_length=128,
+            num_beams=5,
+            early_stopping=True,
+        )[0],
+        skip_special_tokens=True,
+        clean_up_tokenization_spaces=True
+    )
+    return corrected_sentence
+
+import streamlit as st
 
 
-import gradio as gr
-
-
-interface = gr.Interface(fn=correct, 
-                        inputs=gr.Textbox(lines=2, placeholder="Enter sentence here..."),
-                         outputs='text', 
-                        title='Sup, I\'m Gramformer')
-interface.launch()
+st.title('Grammar Correction Tool')
+user_input = st.text_area('Enter your text:')
+if st.button('Correct Grammar'):
+    corrected_text = correct(user_input)
+    st.write('Corrected Text:')
+    st.write(corrected_text)
